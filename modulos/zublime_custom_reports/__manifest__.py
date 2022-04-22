26791877a0b13144007b7b387939147441b6a5be
rrojasvelez <desarrollo3@zabyca.com>
2022-04-22 00:19:03 -0500

    ticket solicitud de presupuesto y ticket orden de compra

---
+++ modulos/zublime_custom_reports/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Zublime Custom Reports',
+    'version': '15.0.1.0.0',
+    'summary': 'Zublime Custom Reports',
+    'category': 'Reports',
+    'author': 'Zublime',
+    'contributors': [
+        'rrojas',
+    ],
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'reports/ticket_cabecera_pie_pagina.xml',
+        'reports/ticket_solicitud_cotizacion_template.xml',
+        'reports/ticket_pedido_compra_template.xml',
+        
+        'reports/paperformat_reports.xml',
+        'reports/purchase_reports.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
