443b281756a9136980c9357ba587337186acdbe3
rrojasvelez <desarrollo3@zabyca.com>
2022-04-22 13:31:20 -0500

    Tickets de sale hechos

--- modulos/zublime_custom_reports/__manifest__.py
+++ modulos/zublime_custom_reports/__manifest__.py
@@ -12,8 +12,10 @@
     ],
     'data': [
         'reports/ticket_cabecera_pie_pagina.xml',
-        'reports/ticket_solicitud_cotizacion_template.xml',
-        'reports/ticket_pedido_compra_template.xml',
+        'reports/ticket_purchase_solicitud_cotizacion_template.xml',
+        'reports/ticket_purchase_pedido_compra_template.xml',
+        'reports/ticket_sale_cotizacion_template.xml',
+        'reports/ticket_sale_order_template.xml',
         
         'reports/paperformat_reports.xml',
         'reports/purchase_reports.xml',
